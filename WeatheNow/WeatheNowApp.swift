b09fb318b6e961b9d1d7084c784878f5d022118c
irinadeeva <deeva97@me.com>
2024-10-24 14:57:56 +0300

    Initial Commit

---
+++ WeatheNow/WeatheNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheNowApp.swift
+//  WeatheNow
+//
+//  Created by Irina Deeva on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatheNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
