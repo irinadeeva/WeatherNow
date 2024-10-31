758eb6b06a67a15aeb5ce9e3be2bd99cddde3c11
irinadeeva <deeva97@me.com>
2024-10-31 11:41:25 +0300

    cleaned Weather Data View code

--- WeatheNow/View/WeatherView/WeatherData.swift
+++ WeatheNow/View/WeatherView/WeatherData.swift
@@ -15,56 +15,38 @@ struct WeatherDataView: View {
         VStack {
             VStack(alignment: .leading, spacing: 5) {
                 Text(weather.name)
-                    .bold()
                     .font(.title)
+                    .bold()
 
                 Text("Today \(Date().formatted(.dateTime.month().day().hour().minute()))")
                     .fontWeight(.light)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
 
-//            VStack {
             VStack(spacing: 0) {
+                Text(weather.main.temp.roundDouble() + "°C")
+                    .font(.system(size: 80))
+                    .fontWeight(.bold)
+                    .padding(.top)
 
-                    Text(weather.main.temp.roundDouble() + "°C")
-                        .font(.system(size: 80))
-                        .fontWeight(.bold)
-                        .padding(.top)
-
-                    VStack(spacing: 0) {
-//                        Image(systemName: "cloud.rain.fill")
-//                            .font(.system(size: 40))
-
-                        AsyncImage(url: URL(string: weather.weather[0].iconUrl)) { image in
-                                        image.resizable()
-//                                .aspectRatio(contentMode: .fit)
-
-                                    } placeholder: {
-
-                                        LoadingView()
-
-                                    }
-                                    .frame(width: 100, height: 100)
-
-                        Text(weather.weather[0].main)
+                VStack(spacing: 0) {
+                    AsyncImage(url: URL(string: weather.weather[0].iconUrl)) { image in
+                        image.resizable()
+                    } placeholder: {
+                        LoadingView()
                     }
+                    .frame(width: 100, height: 100)
+
+                    Text(weather.weather[0].main)
                 }
-//            }
+            }
             .frame(maxWidth: .infinity)
-
-//            Spacer()
-
         }
         .frame(maxHeight: .infinity, alignment: .top)
         .padding()
         .sheet(isPresented: $showSheet) {
-
             WeatherDetailsView(weather: weather)
-                
-
         }
-        
-
     }
 }
 
